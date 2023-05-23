25a304210ef3aa5a9ac0fde9477414b065088a79
Nobleman2704 <turayevasad@gmail.com>
2023-05-23 18:25:17 +0500

    Added auditing

--- src/main/java/com/example/ecommercial/ECommercialApplication.java
+++ src/main/java/com/example/ecommercial/ECommercialApplication.java
@@ -2,8 +2,9 @@ package com.example.ecommercial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class ECommercialApplication {
     public static void main(String[] args) {
         SpringApplication.run(ECommercialApplication.class, args);
