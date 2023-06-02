c9ed9c4aa5ae41b2c4455808d6c687577d04ef54
Nobleman2704 <turayevasad@gmail.com>
2023-06-03 00:36:34 +0500

    added static method inside BaseResponse class

---
+++ src/test/java/com/example/ecommercial/dao/UserDaoTest.java
@@ -0,0 +1,105 @@
+package com.example.ecommercial.dao;
+
+import com.example.ecommercial.ECommercialApplication;
+import com.example.ecommercial.domain.entity.UserEntity;
+import com.example.ecommercial.domain.listener.AuditingAwareImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class UserDaoTest {
+
+    @Autowired
+    private UserDao userDao;
+
+    @MockBean
+    private AuditingAwareImpl auditingAware;
+
+    private UserEntity user;
+
+    private Pageable pageable;
+
+
+    @BeforeEach
+    public void setUp() {
+        user = UserEntity.builder()
+                .name("Asadbek")
+                .username("nobleman")
+                .password("1234")
+                .chatId(123L)
+                .build();
+    }
+
+    @Test
+    void findUserEntityByUsername() {
+        userDao.save(user);
+
+        Optional<UserEntity> optionalUserEntity = userDao
+                .findUserEntityByUsername("nobleman");
+
+        System.out.println("optionalUserEntity.get() = " + optionalUserEntity.get());
+
+
+        assertTrue(optionalUserEntity.isPresent());
+    }
+
+    @Test
+    void findUserEntitiesByChatId() {
+        userDao.save(user);
+        Optional<UserEntity> optionalUser = userDao.findUserEntitiesByChatId(user.getChatId());
+
+        assertTrue(optionalUser.isPresent());
+    }
+
+    @Test
+    void findUserEntitiesByOrderEntitiesIsNotEmptyAndChatIdNotNull() {
+        Pageable pageable = PageRequest.of(0, 5);
+        Page<UserEntity> userEntityPage = userDao
+                .findUserEntitiesByOrderEntitiesIsNotEmptyAndChatIdNotNull(pageable);
+
+        assertNotNull(userEntityPage);
+    }
+
+    @Test
+    void findUserEntitiesByChatIdIsNull() {
+        userDao.save(user);
+        Pageable pageable = PageRequest.of(0, 5);
+        Page<UserEntity> optionalBotUsers = userDao
+                .findUserEntitiesByChatIdIsNull(pageable);
+
+        List<UserEntity> userEntityList = optionalBotUsers.getContent();
+
+        assertTrue(userEntityList.isEmpty());
+    }
+
+    @Test
+    void findUserEntitiesByChatIdIsNotNull(){
+        userDao.save(user);
+        Pageable pageable = PageRequest.of(0, 5);
+        Page<UserEntity> optionalBotUsers = userDao
+                .findUserEntitiesByChatIdIsNotNull(pageable);
+
+        List<UserEntity> userEntityList = optionalBotUsers.getContent();
+
+        assertFalse(userEntityList.isEmpty());
+    }
+}
