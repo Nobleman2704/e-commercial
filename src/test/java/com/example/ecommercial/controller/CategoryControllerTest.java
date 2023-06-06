e36431e3df6f6f82c3ef24f3693556c5d5d1ef9e
tolk1nov <sarvartolqinov13@gmail.com>
2023-06-06 14:47:11 +0500

    added category test

--- src/test/java/com/example/ecommercial/controller/CategoryControllerTest.java
+++ src/test/java/com/example/ecommercial/controller/CategoryControllerTest.java
@@ -1,22 +1,114 @@
 package com.example.ecommercial.controller;
 
+import com.example.ecommercial.ECommercialApplication;
+import com.example.ecommercial.controller.dto.request.CategoryCreateAndUpdateRequest;
+import com.example.ecommercial.dao.ProductCategoryDao;
+import com.example.ecommercial.domain.entity.ProductCategoryEntity;
+import com.example.ecommercial.service.category.CategoryService;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = ECommercialApplication.class)
+@AutoConfigureMockMvc
 
 class CategoryControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ModelMapper modelMapper;
+    @Autowired
+    private ProductCategoryDao categoryDao;
+    @Autowired
+    private CategoryService categoryService;
+
+    private CategoryCreateAndUpdateRequest categoryDto;
 
     @BeforeEach
     void setUp() {
+        categoryDto = CategoryCreateAndUpdateRequest.builder()
+                .name("VIVO")
+                .build();
+
     }
 
+    @SneakyThrows
     @Test
     void addCategory() {
+        ModelAndView modelAndView = mockMvc.perform(
+                post("/category/add")
+                        .with(SecurityMockMvcRequestPostProcessors
+                                .user("nobleman")
+                                .password("1234"))
+                        .flashAttr("category", categoryDto))
+                .andReturn().getModelAndView();
+
+        Map<String, Object> model = modelAndView.getModel();
+
+        String message = (String) model.get("message");
+
+        assertEquals("success", message);
+    }
+    @SneakyThrows
+    @Test
+    void addCategoryFail() {
+        categoryService.save(categoryDto);
+
+        ModelAndView modelAndView = mockMvc.perform(
+                        post("/category/add")
+                                .with(SecurityMockMvcRequestPostProcessors
+                                        .user("nobleman")
+                                        .password("1234"))
+                                .flashAttr("category", categoryDto))
+                .andReturn().getModelAndView();
+
+        Map<String, Object> model = modelAndView.getModel();
+
+        String message = (String) model.get("message");
+
+        assertEquals("category name already exists: " + categoryDto.getName(), message);
     }
 
+    @SneakyThrows
     @Test
     void updateCategory() {
+        categoryDto.setId(1L);
+        categoryService.save(categoryDto);
+
+        categoryDto.setName("REDMI");
+
+        ModelAndView modelAndView = mockMvc.perform(
+                        post("/category/update")
+                                .with(SecurityMockMvcRequestPostProcessors
+                                        .user("nobleman")
+                                        .password("1234"))
+                                .flashAttr("category",categoryDto ))
+                .andReturn().getModelAndView();
+
+        Map<String, Object> model = modelAndView.getModel();
+
+        String message = (String) model.get("message");
+
+        assertEquals("Updated", message);
+
+
     }
 
     @Test
--- src/test/java/com/example/ecommercial/service/user/UserServiceTest.java
+++ src/test/java/com/example/ecommercial/service/user/UserServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
